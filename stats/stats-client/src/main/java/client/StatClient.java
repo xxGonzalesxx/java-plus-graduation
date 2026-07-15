457159dd98bc19cf845f86aad75ee833f68c4876
Stefan <stef.kir1999@gmail.com>
2026-07-15 04:24:04 +0400

    Шаг: 2 готов, обнаружение сервисов

--- stats/stats-client/src/main/java/client/StatClient.java
+++ stats/stats-client/src/main/java/client/StatClient.java
@@ -4,13 +4,15 @@ import ewm.HitDto;
 import ewm.ParamDto;
 import ewm.StatsDto;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClient;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import java.net.URI;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.Optional;
@@ -18,19 +20,31 @@ import java.util.Optional;
 @Slf4j
 @Component
 public class StatClient {
-    final RestClient restClient;
+
+    private static final String STATS_SERVICE_ID = "stats-server";
+
+    private final DiscoveryClient discoveryClient;
+    private final RestClient restClient;
     private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
 
-    public StatClient(@Value("${stats.server.url}") String baseUrl) {
-        this.restClient = RestClient.builder()
-                .baseUrl(baseUrl)
-                .build();
+    public StatClient(DiscoveryClient discoveryClient) {
+        this.discoveryClient = discoveryClient;
+        this.restClient = RestClient.builder().build();
+    }
+
+    private String resolveBaseUrl() {
+        List<ServiceInstance> instances = discoveryClient.getInstances(STATS_SERVICE_ID);
+        if (instances.isEmpty()) {
+            throw new IllegalStateException("Сервис статистики не найден в Eureka: " + STATS_SERVICE_ID);
+        }
+        ServiceInstance instance = instances.get(0);
+        return "http://" + instance.getHost() + ":" + instance.getPort();
     }
 
     public void hit(HitDto hitDto) {
         try {
-            ResponseEntity<Void> response = restClient.post()
-                    .uri("/hit")
+            restClient.post()
+                    .uri(resolveBaseUrl() + "/hit")
                     .contentType(MediaType.APPLICATION_JSON)
                     .body(hitDto)
                     .retrieve()
@@ -48,14 +62,16 @@ public class StatClient {
                 : Optional.of(paramDto.uris());
 
         try {
+            URI uri = UriComponentsBuilder.fromUriString(resolveBaseUrl() + "/stats")
+                    .queryParam("start", paramDto.start().format(formatter))
+                    .queryParam("end", paramDto.end().format(formatter))
+                    .queryParamIfPresent("uris", uris)
+                    .queryParamIfPresent("unique", Optional.ofNullable(paramDto.unique()))
+                    .build()
+                    .toUri();
+
             stats = restClient.get()
-                    .uri(uriBuilder -> uriBuilder
-                            .path("/stats")
-                            .queryParam("start", paramDto.start().format(formatter))
-                            .queryParam("end", paramDto.end().format(formatter))
-                            .queryParamIfPresent("uris", uris)
-                            .queryParamIfPresent("unique", Optional.ofNullable(paramDto.unique()))
-                            .build())
+                    .uri(uri)
                     .header("Content-Type", "application/json")
                     .retrieve()
                     .body(new ParameterizedTypeReference<>() {
