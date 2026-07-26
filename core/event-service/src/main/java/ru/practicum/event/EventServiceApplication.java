6d6d2072e80c99dc6fabc069aef73702e30ec07f
Stefan <stef.kir1999@gmail.com>
2026-07-26 23:46:40 +0400

    Добавлен Feign клиент

--- core/event-service/src/main/java/ru/practicum/event/EventServiceApplication.java
+++ core/event-service/src/main/java/ru/practicum/event/EventServiceApplication.java
@@ -8,8 +8,8 @@ import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@ComponentScan(basePackages = "ru.practicum")
-@EnableFeignClients(basePackages = {"ru.practicum", "client"})
+@EnableFeignClients
+@ComponentScan(basePackages = {"ru.practicum.event"})
 public class EventServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(EventServiceApplication.class, args);
