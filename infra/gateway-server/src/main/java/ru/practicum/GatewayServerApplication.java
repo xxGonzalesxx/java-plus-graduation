4f1340d66d795333818fc217f0a2018f21130c33
Stefan <stef.kir1999@gmail.com>
2026-07-28 22:16:14 +0400

    Ошибка в yaml и properties+actuator

--- infra/gateway-server/src/main/java/ru/practicum/GatewayServerApplication.java
+++ infra/gateway-server/src/main/java/ru/practicum/GatewayServerApplication.java
@@ -7,7 +7,8 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @SpringBootApplication
 @EnableDiscoveryClient
 public class GatewayServerApplication {
+
     public static void main(String[] args) {
-        SpringApplication.run(GatewayServerApplication.class,args);
+        SpringApplication.run(GatewayServerApplication.class, args);
     }
 }
