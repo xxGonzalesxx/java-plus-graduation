3be7772b64c81693a84a5df100ac1aabd107855c
Stefan <stef.kir1999@gmail.com>
2026-08-07 16:57:45 +0400

    проверка динамического порта analyzer

--- stats/analyzer/src/main/java/ru/practicum/analyzer/config/GrpcPortMetadataPublisher.java
+++ stats/analyzer/src/main/java/ru/practicum/analyzer/config/GrpcPortMetadataPublisher.java
@@ -3,8 +3,8 @@ package ru.practicum.analyzer.config;
 import com.netflix.appinfo.ApplicationInfoManager;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
+import org.springframework.grpc.server.lifecycle.GrpcServerStartedEvent;
 import org.springframework.stereotype.Component;
 
 import java.util.HashMap;
@@ -17,10 +17,9 @@ public class GrpcPortMetadataPublisher {
 
     private final ApplicationInfoManager applicationInfoManager;
 
-    @EventListener(ApplicationReadyEvent.class)
-    public void onApplicationReady() {
-        // Получаем порт из пропертей, а не из события
-        int grpcPort = 9096; // или из пропертей
+    @EventListener
+    public void onGrpcServerStarted(GrpcServerStartedEvent event) {
+        int grpcPort = event.getPort();
         log.info("Analyzer: Updating Eureka metadata with gRPC port: {}", grpcPort);
 
         Map<String, String> metadata = new HashMap<>();
